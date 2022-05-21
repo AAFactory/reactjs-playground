7790c1e2e86ca4ee0e0122bc12f3f74a44bcd179
DESKTOP-2T3RPD2\hanjoong.cho <hanjoongcho@gmail.com>
2022-05-21 15:30:37 +0900

    WIP

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
 import Counter2 from './components/Counter2';
+import TodosContainer from './containers/TodosContainer';
 
 function App() {
   return (
     <div>
       <CounterContainer />
       <Counter2 />
+      <hr/>
+      <TodosContainer />
     </div>
   );
 }
