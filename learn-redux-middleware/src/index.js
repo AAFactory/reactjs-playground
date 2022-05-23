003eb3edcc4bf7d03d65667756018c1030cd4bc2
hanjoong.cho <hanjoongcho@gmail.com>
2022-05-23 19:36:23 +0900

    WIP

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -16,7 +16,7 @@ root.render(
   <Provider store={store}>
     <App />
   </Provider>
-);
+);  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
