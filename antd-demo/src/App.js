ee31897c2960aab0b3108fcae07cb96b89d67ea3
hanjoong.cho <hanjoongcho@gmail.com>
2022-05-29 13:29:26 +0900

    WIP

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
 import { useRef, useState, useEffect } from 'react'
-import { Form, Input, Button, Radio, Modal } from 'antd'
+import { Form, Input, Button, Radio, Modal, Space, Tag, Divider } from 'antd'
 import ModalPopup from './components/ModalPopup'
 
 function App() {
@@ -29,74 +29,28 @@ function App() {
 
     useEffect(() => {
       console.log('App.js-useEffect')
-      // if (myRef && myRef.current) {
-      //   const { input } = myRef.current
-      //   input.focus()
-      // }
-      
     })   
-
-    const onFormLayoutChange = ({ layout }) => {
-        setFormLayout(layout)
-    }
-
-    const formItemLayout =
-        formLayout === 'horizontal'
-            ? {
-                  labelCol: {
-                      span: 4,
-                  },
-                  wrapperCol: {
-                      span: 14,
-                  },
-              }
-            : null
-    const buttonItemLayout =
-        formLayout === 'horizontal'
-            ? {
-                  wrapperCol: {
-                      span: 14,
-                      offset: 4,
-                  },
-              }
-            : null
+    
     return (
         <div className="App">
-          {number}
-            <Form
-                {...formItemLayout}
-                layout={formLayout}
-                form={form}
-                initialValues={{ layout: formLayout }}
-                onValuesChange={onFormLayoutChange}
-            >
-                <Form.Item label="Form Layout" name="layout">
-                    <Radio.Group value={formLayout}>
-                        <Radio.Button value="horizontal">
-                            Horizontal
-                        </Radio.Button>
-                        <Radio.Button value="vertical">Vertical</Radio.Button>
-                        <Radio.Button value="inline">Inline</Radio.Button>
-                    </Radio.Group>
-                </Form.Item>
-                <Form.Item label="Field A">
-                    {/* <Input placeholder="input placeholder" ref={myRef} /> */}
-                </Form.Item>
-                <Form.Item label="Field B">
-                    {/* <Input placeholder="input placeholder" /> */}
-                </Form.Item>                
-            </Form>
-            <Button type="primary" onClick={() => openPopup('12312323')}>
-                열람팝업
-            </Button>
-            <Button type="primary" onClick={() => openPopup()}>
-                수정팝업
-            </Button>
-            <Button type="primary" onClick={() => testFun()}>
-                변경
-            </Button>
+            <Divider />
+            <Space>
+                <Tag color="magenta">{number}</Tag>
+                <Button type="primary" onClick={() => testFun()}>
+                    변경
+                </Button>
+            </Space>
+            <Divider />
+            <Space>
+                <Button type="primary" onClick={() => openPopup('12312323')}>
+                    열람팝업
+                </Button>
+                <Button type="primary" onClick={() => openPopup()}>
+                    수정팝업
+                </Button>
+            </Space>
+                       
             <ModalPopup visible={visible} setVisible={setVisible}  workMode={workMode} projectId={projectId} setWorkMode={setWorkMode}></ModalPopup>
-            
         </div>
     )
 }
