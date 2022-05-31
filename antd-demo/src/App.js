c331ad14e220e220b5ebeb8a1ef7dbb796715487
hanjoong.cho <hanjoongcho@gmail.com>
2022-05-31 22:13:57 +0900

    WIP

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Main from './components/Main'
 import TestState from './components/TestState'
+import Basic from './templates/Default'
+import './App.css'
 
 function App() {
     return (
         <>
-            <Route path="/" component={Main} exact={true} />
+            <Route path="/" component={Basic} exact={true} />
             <Route path="/test/state" component={TestState} exact={true} />
         </>
     )
